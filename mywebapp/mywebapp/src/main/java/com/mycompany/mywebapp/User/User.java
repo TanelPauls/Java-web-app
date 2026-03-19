052d3ccc0968cf81c75af4a1bc9ca313d5eccaf7
Tanel Pauls <PaulsTanel@protonmail.com>
2026-03-19 11:41:19 +0200

    add war to build for tomcat

---
+++ mywebapp/mywebapp/src/main/java/com/mycompany/mywebapp/User/User.java
@@ -0,0 +1,8 @@
+package com.mycompany.mywebapp.User;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+}
--- src/test/java/com/mycompany/UserRepositoryTests.java
+++ src/test/java/com/mycompany/UserRepositoryTests.java
@@ -5,66 +5,74 @@ import com.mycompany.user.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
 
 import java.util.Optional;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Rollback(false)
 public class UserRepositoryTests {
-    @Autowired private UserRepository repo;
+
+    @Autowired
+    private UserRepository repo;
+
+    private User createUser(String email) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword("password");
+        user.setFirstName("Test");
+        user.setLastName("User");
+        return repo.save(user);
+    }
 
     @Test
-    public void testAddNew(){
-        User user = new User();
-        user.setEmail("Mannu@testing.no");
-        user.setPassword("Mannu");
-        user.setFirstName("Mannu");
-        user.setLastName("Jeee");
-
-        User savedUser = repo.save(user);
+    public void testAddNew() {
+        User savedUser = createUser("user1@test.com");
 
         Assertions.assertThat(savedUser).isNotNull();
         Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
     }
 
     @Test
-    public void testListAll(){
+    public void testListAll() {
+        createUser("user2@test.com");
+
         Iterable<User> users = repo.findAll();
-        Assertions.assertThat(users).hasSizeGreaterThan(0);
-        for(User user : users){
+
+        Assertions.assertThat(users).isNotEmpty();
+        for (User user : users) {
             System.out.println(user);
         }
     }
 
     @Test
-    public void testUpdate(){
-        Integer userId = 1;
-        Optional<User> optionalUser = repo.findById(userId);
-        User user = optionalUser.get();
-        user.setPassword("Pannu");
+    public void testUpdate() {
+        User user = createUser("user3@test.com");
+
+        user.setPassword("updatedPassword");
         repo.save(user);
 
-        User updatedUser = repo.findById(userId).get();
-        Assertions.assertThat(updatedUser.getPassword()).isEqualTo("Pannu");
+        User updatedUser = repo.findById(user.getId()).orElseThrow();
+        Assertions.assertThat(updatedUser.getPassword()).isEqualTo("updatedPassword");
     }
 
     @Test
-    public void testGet(){
-        Integer userId = 2;
-        Optional<User> optionalUser = repo.findById(userId);
+    public void testGet() {
+        User user = createUser("user4@test.com");
+
+        Optional<User> optionalUser = repo.findById(user.getId());
+
         Assertions.assertThat(optionalUser).isPresent();
         System.out.println(optionalUser.get());
     }
 
     @Test
-    public void testDelete(){
-        Integer userId = 2;
-        repo.deleteById(userId);
-        Optional<User> optionalUser = repo.findById(userId);
+    public void testDelete() {
+        User user = createUser("user5@test.com");
+
+        repo.deleteById(user.getId());
+
+        Optional<User> optionalUser = repo.findById(user.getId());
+
         Assertions.assertThat(optionalUser).isNotPresent();
     }
 }
